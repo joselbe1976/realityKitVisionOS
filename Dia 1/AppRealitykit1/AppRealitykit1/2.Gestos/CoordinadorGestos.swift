0a1d4a518288a32bee70a005d52383664ad5f53c
Jose Luis Bustos <jlbustos@equilibrha.es>
2024-06-07 19:06:26 +0200

    Dia 1

--- Dia 1/AppRealitykit1/AppRealitykit1/2.Gestos/CoordinadorGestos.swift
+++ Dia 1/AppRealitykit1/AppRealitykit1/2.Gestos/CoordinadorGestos.swift
@@ -13,6 +13,7 @@ class CoordinadorGestos: NSObject, ARSessionDelegate {
    weak var view : ARView? //referencia a la vista. Pero ojo Debil.
 
     //Cuando haga TAP en la pantalla... hago algo...
+    /*
     @objc func handleTap(_ recognizer: UITapGestureRecognizer){
         //Desempaqueto la vista y si hay error salimos
         guard let view = self.view else { return }
@@ -43,6 +44,84 @@ class CoordinadorGestos: NSObject, ARSessionDelegate {
             anchorEntity.addChild(box)
             
             view.scene.addAnchor(anchorEntity)
+        }
+    }
+    */
+    //refactor del anterior, añadiendo solo Reality kit
+    /*
+    @objc func handleTap(_ recognizer: UITapGestureRecognizer){
+        //Desempaqueto la vista y si hay error salimos
+        guard let view = self.view else { return }
+        
+        //Al pulsar en la pantalla recibimos la localizacion de la pulsamos
+        let tapLocation = recognizer.location(in: view)
+        
+        // el objeto donde tenemos el TAp (RayCast)
+        let results = view.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .horizontal)
+        
+        //me quedo con el primer TAP
+        if let result = results.first {
+            
+            // ARAnchor = ARkit
+            // AnchorEntity = Reality Kit
+            
+            //Creo un ancla horizontal
+            let anchorEntity = AnchorEntity(raycastResult: result)
+            
+            
+            // creo una box
+            let box = ModelEntity(mesh: MeshResource.generateBox(size: 0.3))
+            box.model?.materials = [SimpleMaterial(color: .blue, isMetallic: true)]
+            
+            //añadir la caja al anchor
+            anchorEntity.addChild(box)
+            
+            //añadir el anchor a la escena
+            view.scene.addAnchor(anchorEntity)
+        }
+    }
+    */
+    
+    //codigo anterior añadiendo gestos.
+    @objc func handleTap(_ recognizer: UITapGestureRecognizer){
+        //Desempaqueto la vista y si hay error salimos
+        guard let view = self.view else { return }
+        
+        //Al pulsar en la pantalla recibimos la localizacion de la pulsamos
+        let tapLocation = recognizer.location(in: view)
+        
+        // el objeto donde tenemos el TAp (RayCast)
+        let results = view.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .horizontal)
+        
+        //me quedo con el primer TAP
+        if let result = results.first {
+            
+            // ARAnchor = ARkit
+            // AnchorEntity = Reality Kit
+            
+            //Creo un ancla horizontal
+            let anchorEntity = AnchorEntity(raycastResult: result)
+            
+            
+            // creo una box
+            let box = ModelEntity(mesh: MeshResource.generateBox(size: 0.3))
+            box.model?.materials = [SimpleMaterial(color: .blue, isMetallic: true)]
+            
+            //Cambio1.
+            box.generateCollisionShapes(recursive: true)
+            
+            
+            //añadir la caja al anchor
+            anchorEntity.addChild(box)
+            
+            //añadir el anchor a la escena
+            view.scene.addAnchor(anchorEntity)
+            
+            
+            //CAMBIO 2. Instalamos en la vista y el objeto,  los gestos que queremos
+            view.installGestures(.all, for: box)
+            
+            
         }
     }
 }
