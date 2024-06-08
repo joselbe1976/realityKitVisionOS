6d912e6cd0c0496b8fae78148166e3090294b81f
Jose Luis Bustos <jlbustos@equilibrha.es>
2024-06-08 17:23:50 +0200

    Finish 2

--- Dia 2/RecnocoimientoImagen/AppRealityKitTemplate/Coordinator.swift
+++ Dia 2/RecnocoimientoImagen/AppRealityKitTemplate/Coordinator.swift
@@ -8,11 +8,33 @@
 import Foundation
 import ARKit
 import RealityKit
+import Combine
 
 class Coordinator: NSObject {
     weak var view: ARView?
+    var cancelable = Set<AnyCancellable>()
     
     func setup() {
+        
+        guard let arView = view else { return }
+        
+        //ancla de la imagen a buscar por la camara
+        let anchor = AnchorEntity(.image(group: "AR Resources", name: "alcachofa"))
+        
+        //Cargamos el robot con Combine
+        ModelEntity.loadAsync(named: "toy_drummer")
+            .sink { completion in
+                if case let .failure(error) = completion {
+                    NSLog("Error al cargar el modelo: \(error)")
+                }
+            } receiveValue: { entity in
+                anchor.addChild(entity)
+                arView.scene.addAnchor(anchor)
+                print("loaded robot")
+            }
+            .store(in: &cancelable)
+
+        
         
     }
     
